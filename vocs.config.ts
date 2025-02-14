bf957c186515fa6fc70bad703db386c9667ffaab
francescocirulli <63744530+francescocirulli@users.noreply.github.com>
2025-02-14 12:28:16 +0100

    Initial documentation setup

---
+++ vocs.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vocs'
+
+export default defineConfig({
+  socials: [
+    {
+      icon: "warpcast",
+      link: "https://warpcast.com/~/channel/betttr",
+    },
+  ],
+  topNav: [
+    { text: "Built by Builders Garden", link: "https://www.builders.garden/" },
+  ],
+  logoUrl: '/betttr-logo.png',
+  title: 'Docs',
+  sidebar: [
+    {
+      text: 'Getting Started',
+      link: '/getting-started',
+    },
+    {
+      text: 'How it works',
+      link: '/how-it-works',
+    }
+  ],
+})
